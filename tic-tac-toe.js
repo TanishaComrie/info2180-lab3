03daa6a46fa522307e0a92518f6cb65bc9bb3119
TanishaComrie <tgcmax@gmail.com>
2023-10-22 15:15:35 -0600

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,9 @@
 function tic_tac(){
-    let tab = document.querySelector("board");
+    let tab = document.getElementById("board");
     tab.classList.add("#board");
-}
-function Square(){
-    let square = document.querySelectorAll("board > div");
-    for (let i = 0; i < square.length; i++){
-        square.classList.add(".square");
-    }
 }
 
 window.onload = function(){
     tic_tac();
-    Square();
 }
+
